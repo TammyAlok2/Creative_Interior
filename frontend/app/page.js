6fbcb0187929efa6758855996f84285ba97f84d1
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-01-18 15:35:02 +0530

    updated home page for deployment

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,37 +1,38 @@
-
-import React from 'react'
-import Head from 'next/head'
-import HeroSection from '@/components/HomePage/Hero'
-import Footer from '@/components/Footer'
-import ProductCategories from '@/components/HomePage/Category'
-import WallpaperGrid from '@/components/HomePage/WallPaper'
-import BlogSection from '@/components/HomePage/Blog'
-import AboutSection from '@/components/HomePage/About'
-import Features from '@/components/HomePage/Features'
-import WallpaperDesign from '@/components/HomePage/Consulantation'
-
+import React from "react";
+import Head from "next/head";
+import HeroSection from "@/components/HomePage/Hero";
+import Footer from "@/components/Footer";
+import ProductCategories from "@/components/HomePage/Category";
+import WallpaperGrid from "@/components/HomePage/WallPaper";
+import BlogSection from "@/components/HomePage/Blog";
+import AboutSection from "@/components/HomePage/About";
+import Features from "@/components/HomePage/Features";
+import WallpaperDesign from "@/components/HomePage/Consulantation";
 
 const page = () => {
   return (
-<>
-<Head>
+    <>
+      <Head>
         <title>Creative Interiors - Your Creative Interior Solution</title>
-        <meta name="description" content="Transform your space with Creative Interiors" />
+        <meta
+          name="description"
+          content="Transform your space with Creative Interiors"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="min-h-screen">
         <HeroSection />
-        <ProductCategories/>
-        <WallpaperGrid/>
-        <AboutSection/>
-        <Features/>
-{/* <WallpaperDesign/> */}
-        <BlogSection/>
-        <Footer/>
+        <ProductCategories />
+        <WallpaperGrid />
+        <AboutSection />
+        <Features />
+        {/* <WallpaperDesign/> */}
+        <BlogSection />
+        <Footer />
       </main>
-</>
-  )
-}
+    </>
+  );
+};
 
-export default page
+export default page;

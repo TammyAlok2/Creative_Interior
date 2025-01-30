4667b0c3fa510bae9eaecef3e4ca2064047c14c4
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-01-31 00:53:28 +0530

    in home page bug of add to cart get solved

--- frontend/src/components/HomePage/WallPaper/ProductCardButton.tsx
+++ frontend/src/components/HomePage/WallPaper/ProductCardButton.tsx
@@ -13,7 +13,7 @@ const CardButton = ({ product }:any) => {
   // console.log("item: ", items)
 
   // Check if the product is already in the cart
-  const isProductInCart = items.some(item => item?.product?.id === product?.id || item?.product?._id === product?._id);
+  const isProductInCart = items.some(item => item?.product?._id === product?._id);
 
   const handleAddToCart = () => {
     addItem(product);
@@ -21,7 +21,7 @@ const CardButton = ({ product }:any) => {
 
   return (
     <div className="flex justify-between mt-5">
-      <Link href={`/singleproduct/${product?._id || product?.id}`}>
+      <Link href={`/singleproduct/${product?._id}`}>
         <button
           type="button"
           className="text-primary hover:text-white border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-light_primary font-small rounded-lg text-sm max-xs:px-5 max-xs:py-2 sm:px-2 sm:py-2 md:px-6 xs:px-5 xs:py-2 max-[1365px]:px-2 lg:px-4 xl:px-5 2xl:px-3 3xl:px-5 lg:py-2.5 text-center mb-2 dark:border-medium_primary dark:text-medium_primary dark:hover:text-white dark:hover:bg-medium_primary dark:focus:ring-primary"

d2921057d3ce3ec9180a1b519af54f27f5dcdda8
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-01-31 12:24:18 +0530

    updated similar products in single product page and navbar

--- frontend/src/components/SingleProduct/ProductInformation/index.jsx
+++ frontend/src/components/SingleProduct/ProductInformation/index.jsx
@@ -147,10 +147,11 @@ const ProductDetail = () => {
                   <input
                     type="number"
                     value={width}
+                    inputmode="numeric" 
                     onChange={(e) => setWidth(e.target.value)}
-                    className="w-full p-2 border rounded-lg"
+                    className="w-full p-2 no-spinner border rounded-lg"
                   />
-                  <span className="absolute right-3 top-2 text-gray-500">
+                  <span className="absolute right-1 top-2 text-gray-500">
                     ft
                   </span>
                 </div>
@@ -163,8 +164,9 @@ const ProductDetail = () => {
                     value={height}
                     onChange={(e) => setHeight(e.target.value)}
                     className="w-full p-2 border rounded-lg"
+                    
                   />
-                  <span className="absolute right-3 top-2 text-gray-500">
+                  <span className="absolute right-1 top-2 text-gray-500">
                     ft
                   </span>
                 </div>
--- frontend/src/components/SingleProduct/SimilarProducts/index.jsx
+++ frontend/src/components/SingleProduct/SimilarProducts/index.jsx
@@ -1,9 +1,14 @@
+'use client'
 import React from "react";
 import WallpaperCard from "@/components/HomePage/WallPaper/WallpaperCard";
 import Wallpapers from "@/data/Wallpaper";
 import Link from "next/link";
+import { useProductDataStore } from "@/stores/productStore";
 
 const SimilarProducts = () => {
+
+  const {product} = useProductDataStore();
+  console.log(product)
   return (
     <section className="py-8 px-4 bg-white">
       {/* Heading */}
@@ -13,13 +18,11 @@ const SimilarProducts = () => {
 
       {/* Responsive Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {Wallpapers?.slice(0, 6)?.map((wallpaper) => (
-            <Link href ="/wallpaper/multiple"> 
+        {product.slice(0, 6)?.map((wallpaper) => (
+            <Link href = {`/wallpaper/multiple/${wallpaper._id}`} key={wallpaper.id}> 
          <WallpaperCard
-         key={wallpaper.id}
-         primary={wallpaper.primary}
-         hover={wallpaper.hover}
-         title={wallpaper.title}
+         key={wallpaper._id}
+    data={wallpaper}
          />
          </Link>
         ))}
